847cd5717350ce9de0212a07074450ca9a463bd4
greschnic <113988717+greschnic@users.noreply.github.com>
2026-04-27 16:04:43 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App' // Убрали расширение .jsx, так Vite надежнее
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
